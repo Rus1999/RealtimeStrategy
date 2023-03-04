2c7c2b11fb5e5db9f06d7db8832cb21d4178f8f2
Rus <punyarit07@gmail.com>
2023-03-05 02:06:22 +0700

    first commit

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnitMovement unitMovement = null;
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+    public UnitMovement GetUnitMovement()
+    {
+        return unitMovement;
+    }
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!isOwned) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!isOwned) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
